68daea0a51e490189288594a2f2ad1eddd486a37
Carlos Portes <carlosportes.c@gmail.com>
2021-04-30 16:32:12 -0400

    2021043002
    -Completed: DB connection. Now testing.

--- inventoryUploader.py
+++ inventoryUploader.py
@@ -27,83 +27,88 @@ for fileName in dirList:
         btsFileList.append(fileName)
     if 'PICO' in fileName:
         picoFileList.append(fileName)
-# BTS File Inventory
-# Get file content
-fileContent = uploaderFunctions.downloadFtpFileString(ftpLogin, testFilePath, btsFileList[0])
-# Move to index 0, to start reading at the beginning.
-fileContent.seek(0)
-# Convert file content to dataframe
-dataframe = pd.read_csv(fileContent)
-hwType = []
-partNumbers = []
-serialNumberList = []
-descList = []
-# Parse df columns as lists (all CSV data is stored as columns)
-data = list(dataframe.columns)
-# Find all keyword's position inside the array
-keywordsDict = {}
-for c in range(len(data)):
-    if '[Cabinet]' in data[c]:
-        keywordsDict['[Cabinet]'] = c
-    if '[Subrack]' in data[c]:
-        keywordsDict['[Subrack]'] = c
-    if '[Slot]' in data[c]:
-        keywordsDict['[Slot]'] = c
-    if '[Board]' in data[c]:
-        keywordsDict['[Board]'] = c
-    if '[Port]' in data[c]:
-        keywordsDict['[Port]'] = c
-    if '[Antenna]' in data[c]:
-        keywordsDict['[Antenna]'] = c
-    if '[HostVer]' in data[c]:
-        keywordsDict['[HostVer]'] = c
-
-# Move index to desired position (depending on document section)
-k = keywordsDict['[Cabinet]'] + 18
-j = k + 5
-i = k + 10
-# Cabinet Section
-while i < keywordsDict['[Subrack]']:
-    hwType.append(data[k])
-    k += 16
-    serialNumberList.append(data[j])
-    j += 16
-    descList.append(data[i])
-    i += 16
-# Subrack Section
-# Move index to desired position (depending on document section)
-k = keywordsDict['[Subrack]'] + 22
-j = k + 7
-i = k + 12
-while i < keywordsDict['[Slot]']:
-    hwType.append(data[k])
-    k += 19
-    serialNumberList.append(data[j])
-    j += 19
-    descList.append(data[i])
-    i += 19
-# Board Section
-# Move index to desired position (depending on document section)
-k = keywordsDict['[Board]'] + 39
-j = k + 5
-i = k + 10
-while i < keywordsDict['[Port]']:
-    hwType.append(data[k])
-    k += 31
-    serialNumberList.append(data[j])
-    j += 31
-    descList.append(data[i])
-    i += 31
 
 # Connect to DB
 connectr = mysql.connector.connect(user = dbPara.dbUsername, password = dbPara.dbPassword, host = dbPara.dbServerIp , database = dbPara.dataTable)
 # Connection must be buffered when executing multiple querys on DB before closing connection.
 pointer = connectr.cursor(buffered=True)
-#for i in hwType:
-#    query = 'INSERT INTO `networkinventory` (`nename`,`hardwaretype`,`serialnumber`,`description`,`lastupdate`) VALUES (\'{}\',\'{}\',\'{}\',\'{}\',\'{}\');'.format()
+# BTS File Inventory
+for fileName in btsFileList:
+    # Get file content
+    fileContent = uploaderFunctions.downloadFtpFileString(ftpLogin, testFilePath, fileName)
+    # Move to index 0, to start reading at the beginning.
+    fileContent.seek(0)
+    # Convert file content to dataframe
+    dataframe = pd.read_csv(fileContent)
+    hwType = []
+    partNumbers = []
+    serialNumberList = []
+    descList = []
+    # Parse df columns as lists (all CSV data is stored as columns)
+    data = list(dataframe.columns)
+    # Find all keyword's position inside the array
+    keywordsDict = {}
+    for c in range(len(data)):
+        if '[Cabinet]' in data[c]:
+            keywordsDict['[Cabinet]'] = c
+        if '[Subrack]' in data[c]:
+            keywordsDict['[Subrack]'] = c
+        if '[Slot]' in data[c]:
+            keywordsDict['[Slot]'] = c
+        if '[Board]' in data[c]:
+            keywordsDict['[Board]'] = c
+        if '[Port]' in data[c]:
+            keywordsDict['[Port]'] = c
+        if '[Antenna]' in data[c]:
+            keywordsDict['[Antenna]'] = c
+        if '[HostVer]' in data[c]:
+            keywordsDict['[HostVer]'] = c
+    # Get NE Name
+    neNameStart = data[0].find('[NEName]')
+    neNameEnd = data[0].find('[NEType]')
+    neName = data[0][neNameStart+8:neNameEnd]
+    # Move index to desired position (depending on document section)
+    k = keywordsDict['[Cabinet]'] + 18
+    j = k + 5
+    i = k + 10
+    # Cabinet Section
+    while i < keywordsDict['[Subrack]']:
+        hwType.append(data[k])
+        k += 16
+        serialNumberList.append(data[j])
+        j += 16
+        descList.append(data[i])
+        i += 16
+    # Subrack Section
+    # Move index to desired position (depending on document section)
+    k = keywordsDict['[Subrack]'] + 22
+    j = k + 7
+    i = k + 12
+    while i < keywordsDict['[Slot]']:
+        hwType.append(data[k])
+        k += 19
+        serialNumberList.append(data[j])
+        j += 19
+        descList.append(data[i])
+        i += 19
+    # Board Section
+    # Move index to desired position (depending on document section)
+    k = keywordsDict['[Board]'] + 39
+    j = k + 5
+    i = k + 10
+    while i < keywordsDict['[Port]']:
+        hwType.append(data[k])
+        k += 31
+        serialNumberList.append(data[j])
+        j += 31
+        descList.append(data[i])
+        i += 31
+
+    for i in range(len(hwType)):
+        query = 'INSERT INTO `alticedr_sitedb`.`networkinventory` (`nename`,`hardwaretype`,`serialnumber`,`description`) VALUES (\'{}\',\'{}\',\'{}\',\'{}\');'.format(neName, hwType[i], serialNumberList[i], descList[i])
+        pointer.execute(query)
+        connectr.commit()
+
 # Close DB Connection
 pointer.close()
 connectr.close()
-print(len(hwType))
-print(len(serialNumberList))
-print(len(descList))
